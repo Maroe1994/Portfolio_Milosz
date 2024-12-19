96151823906d87916eb6c5c556f30f72a4bf97b3
Marek Rogiński <marekroginski1994@gmail.com>
2024-12-19 18:06:52 +0100

    slider 100% comlete

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,71 +3,49 @@ import { ImageBox,Images,SlideWrapper} from "../stylecomponents/Slider.style";
 import { Containeryeyes,Eyeimg } from "../stylecomponents/landingpage.style";
 
 
-export function Slider ({arr,src})  {
-    const[index,setIndex]=useState(0);
-    useEffect(() => {
-        const timer = setInterval(() => {
-          setIndex((prevImage) => (prevImage + 1) % arr.length);
-        }, 3000);
-    
-        return () => {
-          clearInterval(timer);
-        };
-      }, [arr.length]);
-    
-      function nextImage() {
-        setIndex((prevImage) => (prevImage + 1) % arr.length);
-      }
-      const handleDotClick = (i) => {
-    
-        
-    
-     
-    setIndex(i);
-      };
-      
-     /* 
-function previousImage() {
-  setIndex((prevImage) => (prevImage - 1 + arr.length) % arr.length);
-}
-*/
+export function Slider({ arr, src }) {
+  const [index, setIndex] = useState(0);
 
- return(
+  // Automatyczna zmiana zdjęcia co 5 sekund
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prevImage) => (prevImage + 1) % arr.length);
+    }, 5000);
 
-    <SlideWrapper src={src} >
-<ImageBox>
-{arr.length > 0 && (
-            <Images 
-              key={arr[index].src}
-              src={arr[index].src}
-              alt={arr[index].src}
+    return () => {
+      clearInterval(timer);
+    };
+  }, [arr.length]);
+
+  // Funkcja obsługująca kliknięcie w kółko
+  const handleDotClick = (i) => {
+    setIndex(i); // Ustaw indeks na kliknięte kółko
+  };
+return (
+    <SlideWrapper src={src}>
+      <ImageBox>
+        {/* Wyświetlanie aktywnego zdjęcia */}
+        {arr.length > 0 && (
+          <Images
+            key={arr[index].src}
+            src={arr[index].src}
+            alt={arr[index].src}
+          />
+        )}
+
+        {/* Kółka nawigacyjne */}
+        <Containeryeyes>
+          {arr.map((_item, i) => (
+            <Eyeimg
+              key={i}
+              style={{
+                backgroundColor: index === i ? 'white' : 'gray', // Biały kolor dla aktywnego kółka
+              }}
+              onClick={() => handleDotClick(i)} // Kliknięcie zmienia aktywne zdjęcie i kółko
             />
-          )} 
-         <Containeryeyes>
-          {arr.map((_index, i) => (
-            <Eyeimg key={i}   style={{
-              backgroundColor: index===i ? 'white' : 'gray'}} onClick={() => {
-  
-              handleDotClick(index); // Pierwsze zdarzenie
-              
-            
-            nextImage(i); // Drugie zdarzenie
-            }} />
           ))}
         </Containeryeyes>
-          {/* 
-<NextButton className="btnnext" onClick={nextImage}>
-  Previous
-</NextButton>
-<PreviousButton className="btnprevious" onClick={previousImage}>
-  Next
-</PreviousButton> 
-*/}
-
-
-</ImageBox>
-</SlideWrapper>
- )
-
-
+      </ImageBox>
+    </SlideWrapper>
+  );
 }
